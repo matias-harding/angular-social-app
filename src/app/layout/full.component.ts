ddff04873b11d092cdcb51114707fee065690c33
Matias Harding <matias.harding@gmail.com>
2024-03-26 00:36:29 -0300

    Base structure components and modules

---
+++ src/app/layout/full.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full',
+  standalone: true,
+  imports: [],
+  templateUrl: './full.component.html'
+})
+export class FullComponent {
+
+}
